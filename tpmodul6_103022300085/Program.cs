9cc50ff410b6ab762de4d97bdce40b8d737a349d
Devonarya <devonaryadaniswra@gmail.com>
2025-03-23 20:54:31 +0700

    menambahkan class SayaTubeVideo

--- tpmodul6_103022300085/Program.cs
+++ tpmodul6_103022300085/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class SayaTubeVideo
+{
+    private int id;
+    private string title;
+    private int playCount;
+
+    public SayaTubeVideo(string title)
+    { 
+        Random random = new Random();
+        this.id = random.Next(10000, 100000);
+
+        this.title = title;
+        this.playCount = 0;
+    }
+
+    public void IncreasePlayCount(int count)
+    {
+        this.playCount += count;
+    }
+
+    public void PrintVideoDetails()
+    {
+        Console.WriteLine("ID: " + this.id);
+        Console.WriteLine("Title: " + this.title);
+        Console.WriteLine("Play Count: " + this.playCount);
+    }
+
+    public static void Main(string[] args)
+    {
+
+        SayaTubeVideo video = new SayaTubeVideo("Devon Arya Daniswara");
+
+        video.PrintVideoDetails();
+
+        video.IncreasePlayCount(10);
+
+        Console.WriteLine("\n");
+        video.PrintVideoDetails();
+    }
+}
